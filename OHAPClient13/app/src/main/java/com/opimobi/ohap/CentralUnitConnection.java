6924534a2e3414069b8aee81957d526c69a0b1d9
Aapo Keskimölö <aapokesk@gmail.com>
2016-04-01 20:29:32 +0300

    v1.0 - Initial Version
    Summary:
    Implemented MainActivity with initial layout, OHAP server with 1 dummy device, text/bar/switch widgets and implemented listener for changing the binary value in the dummy device
    
    Status:
    Doings1 (almost finished)

--- OHAPClient13/app/src/main/java/com/opimobi/ohap/CentralUnitConnection.java
+++ OHAPClient13/app/src/main/java/com/opimobi/ohap/CentralUnitConnection.java
@@ -1,10 +1,25 @@
 package com.opimobi.ohap;
 
 /**
- * Created by Aapo Keskimolo on 29.3.2016.
+ * Implementation of central unit connection in an OHAP application.
+ *
+ * The central unit (superclass) has an URL that is used when connecting to the OHAP server.
+ *
+ * All items will register themselves automatically to the central unit they belong to. The
+ * central unit provides {@link #getItemById(long)} method to retrieve an item from any
+ * level of the container hierarchy by specifying the identifier of the item.
+ *
+ * Change history:
+ * v1.0     Aapo Keskimolo      Initial version, implemented superclass abstract methods
+ *
+ * @see com.opimobi.ohap.CentralUnit
+ *
+ * @author Aapo Keskimolo &lt;aapokesk@gmail.com>
+ * @version 1.0
  */
 
 
+import android.util.Log;
 import java.net.URL;
 
 
@@ -14,22 +29,28 @@ public class CentralUnitConnection extends CentralUnit {
         super(newUrl);
     }
 
-
     @Override
     protected void listeningStateChanged(Container container, boolean listening) {
-        if (listening)
+
+        if (listening) {
             container.startListening();
-        else
+            System.out.println("OHAP Server is listening to connections.");
+        }
+        else {
             container.stopListening();
+            System.out.println("OHAP Server stopped listening to connections.");
+        }
     }
 
     @Override
     protected void changeBinaryValue(Device device, boolean value) {
         device.changeBinaryValue(value);
+        Log.i("DEBUG", "Binary value changed: " + value);
     }
 
     @Override
     protected void changeDecimalValue(Device device, double value) {
         device.changeDecimalValue(value);
+        Log.i("DEBUG", "Decimal value changed: " + value);
     }
 }
--- OHAPClient13/app/src/main/java/fi/oulu/tol/esde_2016_013/ohapclient13/DeviceActivity.java
+++ OHAPClient13/app/src/main/java/fi/oulu/tol/esde_2016_013/ohapclient13/DeviceActivity.java
@@ -1,41 +1,105 @@
 package fi.oulu.tol.esde_2016_013.ohapclient13;
 
+/**
+ * Device activity for an OHAP application.
+ *
+ * Change history:
+ * v1.0     Aapo Keskimolo      Initial version with layout, OHAP server, 1 dummy device, widgets and listeners
+ *
+ * @author Aapo Keskimolo &lt;aapokesk@gmail.com>
+ * @version 1.0
+ */
+
+import com.opimobi.ohap.*;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.opimobi.ohap.*;
-
-import java.net.MalformedURLException;
-import java.net.URL;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 
-public class DeviceActivity extends ActionBarActivity {
 
+public class DeviceActivity extends ActionBarActivity implements CompoundButton.OnCheckedChangeListener {
+
+    // Widgets
+    private TextView textViewContainerName = null;
+    private TextView textViewDeviceName = null;
+    private TextView textViewDeviceDesc = null;
+    private SeekBar seekBar = null;
+    private Switch switch1 = null;
+    private TextView textViewSwitchValue = null;
+
+    // OHAP applications
+    CentralUnit centralUnit = null;
+    Device dummyDevice = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        // Loading XML layout
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device);
 
-        CentralUnit centralUnit = null;
+        // Instantiating widgets
+        // note: findViewById returns View object, which is a super class of Widget class. Therefore,
+        // all view objects need to be down-cast to target Widgets objects.
+        textViewContainerName = (TextView)(findViewById(R.id.textViewContainerName));
+        textViewDeviceName = (TextView)(findViewById(R.id.textViewDeviceName));
+        textViewDeviceDesc = (TextView)(findViewById(R.id.textViewDeviceDesc));
+
+        // Instantiating seekbar (for changing decimal value)
+        seekBar = (SeekBar)(findViewById(R.id.seekBar));
+        // TODO Implement listener for Seekbar and method for changing the decimal value on the text widget above. You may use the Switch -method as an example. Hint: Google "Seekbar listener"
+        // Registering new listener for the switch bar action
+        // ...
+        // Instatiating widget for displaying the current decimal value
+        // ...
+
+        // Instantiating switch object for changing binary value)
+        switch1 = (Switch)(findViewById(R.id.switch1));
+        // Registering new listener for the switch bar action
+        switch1.setOnCheckedChangeListener(this);
+        // Instatiating widget for displaying the current binary value
+        textViewSwitchValue = (TextView)(findViewById(R.id.textViewSwitch));
+
+        // Instantiating OHAP server
+//        CentralUnit centralUnit = null;
         try {
+            // Java enforces try-catch statement for MalformedURLException
             centralUnit = new CentralUnitConnection(new URL("http://ohap.opimobi.com:8080/"));
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
         centralUnit.setName("OHAP Test Server");
 
-        Device device = new Device(centralUnit, 1, Device.Type.ACTUATOR, Device.ValueType.BINARY);
-        device.setName("Dumbo Jumbo");
+        // Instantiating a dummy
+        // device with dummy values
+        dummyDevice = new Device(centralUnit, 1, Device.Type.ACTUATOR, Device.ValueType.BINARY);
+        dummyDevice.setName("Dummy Device");
+        dummyDevice.setDescription("Description of the Dummy Device");
+        dummyDevice.setBinaryValue(false);
 
+        // set values on the TextView objects
+        textViewDeviceName.setText(dummyDevice.getName());
+        textViewDeviceDesc.setText(dummyDevice.getDescription());
+        // TODO The top TextView should show the container hierarchy (not documented -> ask teacher)
+        textViewContainerName.setText("Container hierarchy (TBD)");
     }
 
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        // Implementation for the method that is called whenever the binary Switch value has changed
+        dummyDevice.setBinaryValue(isChecked);
+        textViewSwitchValue.setText(Boolean.toString(dummyDevice.getBinaryValue())); // display device value
+    }
+
+    // TODO Add SeekBar call method(s) here
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,11 +115,12 @@ public class DeviceActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
